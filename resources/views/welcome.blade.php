327fe26bfcce76f49b5994c803389d21b4384da5
acegaf74 <mzidqiassegaf@gmail.com>
2025-11-09 14:54:29 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bismo</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="max-h-screen">
+    <main class="justify-between">
+        <header class="">
+            <h1 class="text-3xl font-bold underline">Header</h1>
+        </header>
+        <nav class="">
+            <ul>
+                <li><a href="">Nav 1</a></li>
+                <li><a href="">Nav 2</a></li>
+                <li><a href="">Nav 3</a></li>
+            </ul>
+        </nav>
+        <article class="">
+            <h1>Main article area</h1>
+            <p>Article paragraph</p>
+        </article>
+        <aside class="">Sidebar</aside>
+        <div class="">Advertising</div>
+        <footer class="">Footer</footer>
+    </main>
+</body>
+</html>
