485da6063af2832c3acc3dc52708bcbc8f152bd2
acegaf74 <mzidqiassegaf@gmail.com>
2025-11-14 21:25:54 +0700

    0.001: testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,9 +13,8 @@
         </header>
         <nav class="">
             <ul>
-                <li><a href="">Nav 1</a></li>
-                <li><a href="">Nav 2</a></li>
-                <li><a href="">Nav 3</a></li>
+                <li><a href="/login">Login</a></li>
+                <li><a href="/register">Register</a></li>
             </ul>
         </nav>
         <article class="">
