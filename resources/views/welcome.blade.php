678f497cb82bae2f02d614ce5da204ecde734465
acegaf74 <mzidqiassegaf@gmail.com>
2025-11-29 11:32:01 +0700

    0.002: rename branch and app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,27 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bismo</title>
+    <title>{{ config('app.name') }}</title>
     @vite('resources/css/app.css')
 </head>
-<body class="max-h-screen">
-    <main class="justify-between">
-        <header class="">
-            <h1 class="text-3xl font-bold underline">Header</h1>
-        </header>
+<body class="max-h-screen grid-[1fr_auto_1fr]">
+    <header class="">
+        <h1 class="text-3xl font-bold underline">Header</h1>
         <nav class="">
             <ul>
                 <li><a href="/login">Login</a></li>
                 <li><a href="/register">Register</a></li>
             </ul>
         </nav>
-        <article class="">
-            <h1>Main article area</h1>
-            <p>Article paragraph</p>
-        </article>
+    </header>
+    <main class="">
+        <form action="">
+            <label for="">Dropdown</label>
+            <div class="">
+                <select name="" id="">
+                    <option>Option 1</option>
+                    <option>Option 2</option>
+                    <option>Option 3</option>
+                    <option>Option 4</option>
+                </select>
+                <input type="submit" value="Submit">
+            </div>
+        </form>
         <aside class="">Sidebar</aside>
         <div class="">Advertising</div>
-        <footer class="">Footer</footer>
     </main>
+    <footer class="">Footer</footer>
 </body>
 </html>
